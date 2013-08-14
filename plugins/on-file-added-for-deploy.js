0fd884594c3df98cb77b9b816541e122652e5bab
magicsky <garcia.relax@gmail.com>
2013-08-14 17:09:42 +0800

    增加目录的同步

--- plugins/on-file-added-for-deploy.js
+++ plugins/on-file-added-for-deploy.js
@@ -5,6 +5,7 @@
  * 为了远程部署准备的文件创建时的回调
  */
 
+var fs = require("fs");
 var path = require("path");
 var util = require("util");
 
@@ -12,6 +13,8 @@ var Connection = require("ssh2");
 var log4js = require('log4js');
 var nodeproxy = require('nodeproxy');
 var StringUtils = require("underscore.string");
+var findSync = require("findit");
+var EventEmitter = require('events').EventEmitter;
 
 module.exports = function(config, filePath, stat) {
     var logger = log4js.getLogger("watcher");
@@ -23,11 +26,72 @@ module.exports = function(config, filePath, stat) {
     connection.on("ready", onConnectReady);
 
     function onConnecting() {
-        logger.info("on ssh connecting");
+        logger.debug("on ssh connecting");
+    }
+
+    /**
+     * 拼装出远程的文件路径
+     */
+    function joinRemoteFile(filePath) {
+        var postfix = StringUtils.lstrip(filePath, {source: config.localPath});
+        postfix = StringUtils.lstrip(postfix, {source: path.sep});
+        var remoteFile = path.join(config.remotePath, postfix);
+        remoteFile = remoteFile.split(path.sep).join(config.remotePathSep);
+        return remoteFile;
+    }
+
+    /**
+     * 创建目录
+     */
+    function makeRootDirectory(dirName, fileNames, syncEmitter, remoteFile, sftp) {
+        var command = util.format("mkdir -pv %s", dirName);
+        logger.debug(command);
+        connection.exec(command, function(error, stream) {
+            if (error) {
+                logger.error(error);
+                return;
+            }
+            stream.on("exit", function() {
+                syncEmitter.emit("next", fileNames, syncEmitter, remoteFile, sftp);
+            });
+        });
+    }
+
+    function onTriggerNextSync(fileNames, syncEmitter, remoteFile, sftp) {
+        if (!fileNames || fileNames.length <= 0) {
+            syncEmitter.removeListener("next", function() {});
+            nodeproxy(onFastPutCallback, {
+                sftp: sftp,
+                connection: connection,
+                remoteFile: remoteFile
+            })();
+            return;
+        }
+
+        var fileName = fileNames.shift();
+        var remoteFile = joinRemoteFile(fileName);
+        var command = null;
+        logger.info(util.format("syncing %s to %s", fileName, remoteFile));
+        if (fs.statSync(fileName).isDirectory()) {
+            command = util.format("mkdir -pv %s", remoteFile);
+            connection.exec(command, function(error, stream) {
+                stream.on("exit", onTriggerNextSync);
+            });
+        }
+        else if (fs.statSync(fileName).isFile()) {
+            command = util.format(
+                "mkdir -pv %s", path.dirname(remoteFile));
+            connection.exec(command, function(error, stream) {
+                sftp.fastPut(fileName, remoteFile, function() {
+                    syncEmitter.emit("next",
+                        fileNames, syncEmitter, remoteFile, sftp);
+                });
+            });
+        }
     }
 
     function onConnectReady() {
-        logger.info("on ssh connect ready");
+        logger.debug("on ssh connect ready");
         connection.sftp(function(error, sftp) {
             if (error) {
                 logger.error("sftp open error: " + error);
@@ -35,36 +99,31 @@ module.exports = function(config, filePath, stat) {
             }
 
             sftp.on("end", function() {
-                logger.info("sftp session end");
+                logger.debug("sftp session end");
             });
-            var postfix = StringUtils.lstrip(filePath, {source: config.localPath});
-            postfix = StringUtils.lstrip(postfix, {source: path.sep});
-            var remoteFile = path.join(config.remotePath, postfix);
-            remoteFile = remoteFile.split(path.sep).join(config.remotePathSep);
-            logger.info(util.format('ready to scp %s to %s', filePath, remoteFile));
+
+            var remoteFile = joinRemoteFile(filePath);
+            logger.debug(util.format('ready to sync %s to %s', filePath, remoteFile));
+
             // TODO garcia.wul 这块用ssh2做起来比较麻烦，暂时先调用命令
             var command = util.format("rm -rf %s", remoteFile);
-            logger.info(command);
+            logger.debug(command);
+
             connection.exec(command, function(error, stream) {
                 stream.on("exit", function(code, signal) {
                     if (stat.isDirectory()) {
-                        command = util.format("mkdir -pv %s", remoteFile);
-                        logger.info(command);
-                        connection.exec(command, function(error, stream) {
-                            stream.on("exist", function() {
-                                nodeproxy(onFastPutCallback, {
-                                    sftp: sftp,
-                                    connection: connection,
-                                    remoteFile: remoteFile
-                                })();
-                            });
-                        });
+                        var fileNames = findSync.sync(filePath);
+                        var syncEmitter = new EventEmitter();
+                        syncEmitter.addListener("next", onTriggerNextSync);
+                        makeRootDirectory(filePath, fileNames, syncEmitter,
+                            remoteFile, sftp);
                     }
                     else if (stat.isFile()) {
                         command = util.format("mkdir -pv %s", path.dirname(remoteFile));
-                        logger.info(command);
+                        logger.debug(command);
                         connection.exec(command, function(error, stream) {
-                            sftp.fastPut(filePath, remoteFile, nodeproxy(onFastPutCallback, {
+                            sftp.fastPut(filePath, remoteFile,
+                                nodeproxy(onFastPutCallback, {
                                 sftp: sftp,
                                 connection: connection,
                                 remoteFile: remoteFile
@@ -93,11 +152,11 @@ module.exports = function(config, filePath, stat) {
     }
 
     function onConnectEnd() {
-        logger.info("on ssh connect end");
+        logger.debug("on ssh connect end");
     }
 
     function onConnectClosed() {
-        logger.info("on ssh connect closed");
+        logger.debug("on ssh connect closed");
     }
 
     connection.connect({
