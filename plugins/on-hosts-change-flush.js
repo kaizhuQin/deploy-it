094e0bd0e19d9d07558a3f0038ac8d87034d23f4
magicsky <garcia.relax@gmail.com>
2013-08-14 15:18:31 +0800

    增加一个监听hosts，然后自动flush dns cache的plugin

---
+++ plugins/on-hosts-change-flush.js
@@ -0,0 +1,40 @@
+/**
+ * User: garcia.wul (garcia.wul@alibaba-inc.com)
+ * Date: 13-8-14
+ * Time: 下午1:44
+ * 用于监听hosts文件后，刷新dns缓存
+ */
+
+var exec = require('child_process').exec;
+
+var log4js = require('log4js');
+var logger = log4js.getLogger("on-hosts-change-flush");
+var os = require("os");
+module.exports = function(config, filePath, stat) {
+    var flushDnsCommand = null;
+    logger.info("platform is " + os.platform());
+    if (os.platform() === "darwin") {
+        flushDnsCommand = "sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder";
+    }
+    else if (os.platform() === "linux") {
+        flushDnsCommand = "sudo /etc/init.d/nscd restart";
+    }
+    else if (os.platform() === "win32") {
+        flushDnsCommand = "ipconfig /flushdns";
+    }
+    if (flushDnsCommand === null) {
+        return;
+    }
+    exec(flushDnsCommand, function(error, stdout, stderr) {
+        if (error) {
+            logger.error(error);
+        }
+        if (stderr) {
+            logger.error(stderr);
+        }
+        if (stdout) {
+            logger.info(stdout)
+        }
+    });
+
+};
